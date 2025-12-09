de58e672bf19783880d3881a55bd9993b16361c4
markylaredo <markanthonylaredo@gmail.com>
2025-12-09 10:16:14 +0800

    Add favicon, robots.txt, and TypeScript configuration
    - Added a favicon.ico file to the public directory.
    - Created a robots.txt file to manage web crawler access.
    - Introduced a tsconfig.json file for TypeScript configuration in the project.

---
+++ app/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <UContainer>
+    <UInput v-model="text" />
+    <USelect v-model="variant" :items="variants" />
+
+    <img style="margin: 30px;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;border-radius: 10px;" class="w-full h-full " width="300" :src="qr" alt="QR Code" />
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+import type { SVGVariant } from "nuxt-qrcode";
+
+const text = ref("Hello World");
+const variant = ref<SVGVariant>("dots");
+const variants = ref(["default", "circle", "pixelated", "rounded", "dots"]);
+
+const qr = useQrcode(text, {
+  toBase64: true,
+ variant: {
+        inner: 'dots',
+        marker: 'rounded',
+        pixel: 'rounded',
+      },
+   radius: 1,
+  blackColor: "currentColor",
+  whiteColor: "#ffff",
+});
+</script>
